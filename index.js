3aaee1d4e82a0334dccfe13354c8b7a01166f0ec
Maikol <maikvas666@gmail.com>
2025-09-28 10:18:35 -0500

    actualizando js y html para formularios

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
+
+
+// Formulario de envio
+const miFormulario = document.getElementById('miFormulario');
+function getDataInfo() {
+
+  miFormulario.reset();
+
+  modal_gracias = document.getElementById('modalGracias')
+
+  modal_gracias.showModal();
+
+}
+
+
